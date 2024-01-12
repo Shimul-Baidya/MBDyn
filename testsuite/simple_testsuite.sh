e76e6e4f6bf735de4d1043858e66d01e91d8cca7
Reinhard Resch <mbdyn-user@a1.net>
2024-01-12 21:07:13 +0100

    Some processes were still not killed, despite of the --kill-after flag.

--- testsuite/simple_testsuite.sh
+++ testsuite/simple_testsuite.sh
@@ -277,7 +277,7 @@ for mbd_filename in `find ${mbdyn_testsuite_prefix_input} '(' ${search_expressio
             *)
                 echo "timeout after ${mbdyn_testsuite_timeout}"
                 ## Octave will not dump a core file on SIGINT
-                mbd_command="timeout --signal=SIGTERM --kill-after=${mbdyn_testsuite_timeout} ${mbdyn_testsuite_timeout} ${mbd_command}"
+                mbd_command="timeout --signal=SIGKILL ${mbdyn_testsuite_timeout} ${mbd_command}"
             ;;
         esac
 
