9c43c0648da40bc83ad8083e0bc0a032ea544a82
Shimul-Baidya <shimul.cse31@gmail.com>
2025-10-19 00:00:31 +0600

    (RPM/components): export core classes in package namespace

--- contrib/PythonPreprocessor/RPM/components/__init__.py
+++ contrib/PythonPreprocessor/RPM/components/__init__.py
@@ -1,1 +1,16 @@
 # This file marks the components directory as a Python package.
+
+from .base import RotorcraftComponent, Rotorcraft
+from .airframe import Airframe
+from .rotor import Rotor
+from .hub import Hub
+from .mast import Mast
+
+__all__ = [
+    'RotorcraftComponent',
+    'Rotorcraft',
+    'Airframe',
+    'Rotor',
+    'Hub',
+    'Mast',
+]
